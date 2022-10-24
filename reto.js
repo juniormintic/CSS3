8f3353d996252c78c27cec43598a238acc45d48f
junior <jhgalvanmintic@gmail.com>
2022-10-24 13:03:10 -0500

    subir archivos a git hub

---
+++ reto.js
@@ -0,0 +1,37 @@
+
+// const cadena="1 5 9 6 3 7 -1 -4";
+
+// function mayoMenor(cadena){
+//     let array=cadena.split(" ").sort();
+//     return `${array[array.length-1]} ${array[0]}`
+// };
+
+// const res=mayoMenor(cadena);
+
+// console.log(res);
+
+const numero= 39;
+
+/* function persistencia(num){    
+
+       while(num>9){
+        num=num.toString().split("").map(item=> parseInt(item)).reduce((a,b)=> a*=b)        
+       }       
+    
+    return num
+      
+
+
+}
+
+console.log(persistencia(numero)) */
+/* 
+function solution(array) {
+    // Tu código aquí 👈 
+    return array.filter((item,i,array)=> array.indexOf(item)===i).sort((a,b)=>a-b)
+}; 
+console.log(solution([1, 2, -3, -1, 2, 4, 4, 1, 45, -1])); */
+
+
+
+
